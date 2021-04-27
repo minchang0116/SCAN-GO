f55a6f4cbe78bddbc5265b4dfa6bdc2e7ef3bfa9
곽충섭 <cndtjq145@naver.com>
2021-04-27 17:44:48 +0900

    Add S04P31D101-104 : 회원가입 중복 체크 및 비밀번호 체크, 입력 체크

--- frontend/src/pages/RegisterForm.js
+++ frontend/src/pages/RegisterForm.js
@@ -1,144 +1,377 @@
-import React, { useState } from 'react';
-import { TextInput, StyleSheet, View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import React, {useState} from 'react';
+import {
+  TextInput,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 
 const RegisterForm = () => {
-    const [idCheck, setIdCheck] = useState(false)
-    const [pwCheck, setPwCheck] = useState(false)
-    return(
-        <ScrollView 
-        showsVerticalScrollIndicator={false}
-        style={styles.container}>
-            <View style={styles.inputForm}>
-                <View>
-                    <Text style={styles.titleText}>ID(Email)</Text>
-                    <View style={styles.inputWithBtnArea}>
-                        <TextInput style={styles.inputWithBtn} placeholder=" @example.com"/>
-                        <TouchableOpacity
-                            style={styles.CheckBtn}
-                        >
-                            <Text style={styles.btnText}>중복체크</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View>
-                        {idCheck ? (<Text style={styles.afterCheck}>사용가능한 ID 입니다!</Text>) 
-                        : <Text style={styles.preCheck} >ID 중복체크를 해주세요.</Text>}
-                    </View>
-                </View>
-                <Text style={styles.titleText}>생년월일</Text>
-                <TextInput style={styles.textInput} placeholder=" xxxx.xx.xx"/>
-                <View>
-                    <Text style={styles.titleText}>비밀 번호</Text>
-                    <TextInput style={styles.textInput} placeholder=" 특수문자를 포함한 8자리 이상 20자리 이하"/>
-                    <Text style={styles.titleText}>비밀 번호 확인</Text>
-                    <TextInput style={[styles.textInput, {marginBottom:10}]} placeholder=" 다시 한번 입력해주세요"/>
-                    <View>
-                        {pwCheck ? (<Text style={styles.afterCheck}>비밀번호가 일치합니다!</Text>) 
-                        : <Text style={styles.preCheck} >비밀번호가 일치하지 않습니다!</Text>}
-                    </View>
-                </View>
-                <Text style={styles.titleText}>휴대 전화</Text>
-                <View style={styles.inputWithBtnArea}>
-                        <TextInput style={styles.inputWithBtn} placeholder=" xxx-xxxx-xxxx"/>
-                        <TouchableOpacity
-                            style={styles.CheckBtn}
-                        >
-                            <Text style={styles.btnText}>번호전송</Text>
-                        </TouchableOpacity>
-                </View>
-                <Text style={[styles.titleText, {marginTop:10}]}>인증 번호</Text>
-                <View style={styles.inputWithBtnArea}>
-                        <TextInput style={styles.inputWithBtn} placeholder=" 인증번호를 입력해주세요"/>
-                        <Text style={styles.timerText}>05:00</Text>
-                </View>
-            </View>
-            <View style={styles.loginBtnArea}>
-                <TouchableOpacity
-                    style={styles.loginBtn}
-                >
-                    <Text style={styles.loginText}>로그인</Text>
-                </TouchableOpacity>
-            </View>
-        </ScrollView>
-    )
-}
+  // 유저 정보
+  // 아이디
+  const [idCheck, setIdCheck] = useState(false);
+  const [id, setId] = useState('');
+  const [isduplicated_id, setDid] = useState(false);
+  // 생일
+  const [birthCheck, setbirthCheck] = useState(false);
 
-export default RegisterForm
+  // 비밀번호
+  const [pwCheck, setPwCheck] = useState(false);
+  const [pwFormCheck, setpwFormCheck] = useState(false);
+  const [password, setPassword] = useState('');
+  const [confirm_password, setConfirmPassword] = useState('');
+
+  // 휴대전화
+  const [cellnumber, setCellNum] = useState('');
+  const [cellCheck, setCellCheck] = useState(false);
+  const [isduplicated_cell, setDcell] = useState(false);
+
+  // 빈칸 관리
+  const [idBlank, setIdBlank] = useState(true);
+  const [birthBlank, setBirthBlank] = useState(true);
+  const [pwFormBlank, setPwFormBlank] = useState(true);
+  const [pwBlank, setPwBlank] = useState(true);
+  const [cellBlank, setCellBlank] = useState(true);
+
+  // 아이디 입력 확인
+  const onIdInputHandler = text => {
+    let id = text;
+    setId(id);
+    setIdCheck(false);
+    if (id === '') {
+      setIdBlank(true);
+      return;
+    }
+    setIdBlank(false);
+  };
+
+  // 아이디 중복 체크
+  const onIdCheckHandler = () => {
+    if (id === '') {
+      alert('ID를 입력해주세요.');
+      return;
+    }
+    // back에 아이디 있는지 확인 체크
+    setIdCheck(true);
+    setDid(false);
+  };
+
+  // 생일 입력 체크
+  const onBirthCheckHandler = text => {
+    let birth = text;
+    if (birth === '') {
+      setBirthBlank(true);
+      return;
+    }
+    setBirthBlank(false);
+    if (birth.length != 8) {
+      setbirthCheck(false);
+      return;
+    }
+    setbirthCheck(true);
+  };
+
+  // 비밀번호 양식 체크
+  const onPasswordHandler = text => {
+    let pw = text;
+    setPassword(pw);
+
+    if (pw !== confirm_password) {
+      setPwCheck(false);
+    } else {
+      setPwCheck(true);
+    }
+
+    if (text === '') {
+      setPwFormBlank(true);
+      return;
+    }
+
+    setPwFormBlank(false);
+
+    let num = pw.search(/[0-9]/g);
+
+    let eng = pw.search(/[a-zA-Z]/gi);
+
+    let spe = pw.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+
+    if (pw.length < 8 || pw.length > 20) {
+      // 길이 체크
+      setpwFormCheck(false);
+      return;
+    }
+
+    if (pw.search(/ /) != -1) {
+      // 공백 여부 체크
+      setpwFormCheck(false);
+      return;
+    }
+    if (num < 0 || eng < 0 || spe < 0) {
+      // 특수문자 체크
+      setpwFormCheck(false);
+      return;
+    }
+    // 가능
+    setpwFormCheck(true);
+  };
+
+  // 비밀번호 확인
+  const onPasswordConfirmHandler = text => {
+    let confirmPw = text;
+    setConfirmPassword(confirmPw);
+    if (text === '') {
+      setPwCheck(false);
+      setPwBlank(true);
+      return;
+    }
+
+    setPwBlank(false);
+
+    if (confirmPw !== password) {
+      setPwCheck(false);
+      return;
+    }
+    setPwCheck(true);
+  };
+
+  // 휴대전화 입력
+  const onCellInputHandler = text => {
+    let cell = text;
+    setCellNum(cell);
+    setCellCheck(false);
+    if (cell === '') {
+      setCellBlank(true);
+      return;
+    }
+    setCellBlank(false);
+  };
+  // 휴대전화 중복 체크
+  const onCellCheckHandler = () => {
+    if (cellnumber === '') {
+      setCellBlank(true);
+      alert('휴대전화를 입력해주세요.');
+      return;
+    }
+
+    // back에 휴대전화 있는지 확인 체크
+    setCellCheck(true);
+    setDcell(false);
+  };
+
+  // 모든 정보가 제대로 입력되었는지 확인
+  const checkAllRegisterInfoHandler = () => {
+    if (!idCheck) {
+      alert('ID 중복체크를 해주세요!');
+      return;
+    }
+    if (!birthCheck) {
+      alert('생일을 입력해주세요!');
+      return;
+    }
+    if (!pwFormCheck) {
+      alert('비밀번호 양식을 확인해주세요!');
+      return;
+    }
+    if (!pwCheck) {
+      alert('비밀번호를 다시 확인해주세요!');
+      return;
+    }
+    if (!cellCheck) {
+      alert('휴대전화 인증을 해주세요!');
+      return;
+    }
+    // Back 통신
+    alert('회원가입이 완료되었습니다.');
+  };
+
+  return (
+    <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
+      <View style={styles.inputForm}>
+        <View>
+          <Text style={styles.titleText}>ID(Email)</Text>
+          <View style={styles.inputWithBtnArea}>
+            <TextInput
+              autoCompleteType={'email'}
+              style={styles.inputWithBtn}
+              placeholder="example@gmail.com"
+              onChangeText={text => onIdInputHandler(text)}
+            />
+            <TouchableOpacity
+              style={styles.CheckBtn}
+              onPress={onIdCheckHandler}>
+              <Text style={styles.btnText}>중복체크</Text>
+            </TouchableOpacity>
+          </View>
+          <View>
+            {idBlank ? null : idCheck ? (
+              isduplicated_id ? ( // 중복 여부
+                <Text style={styles.afterCheck}>ID가 중복되었습니다!</Text>
+              ) : (
+                <Text style={styles.afterCheck}>사용가능한 ID 입니다!</Text>
+              )
+            ) : ( // 중복체크
+              <Text style={styles.preCheck}>ID 중복체크를 해주세요.</Text>
+            )}
+          </View>
+        </View>
+        <View>
+          <Text style={styles.titleText}>생년월일</Text>
+          <TextInput
+            style={[styles.textInput, {marginBottom: 6}]}
+            placeholder="생년월일(20210427)"
+            onChangeText={onBirthCheckHandler}
+          />
+          {birthBlank ? null : birthCheck ? null : (
+            <Text style={styles.afterCheck}>생일을 확인해주세요</Text>
+          )}
+        </View>
+        <View>
+          <Text style={styles.titleText}>비밀 번호</Text>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={text => onPasswordHandler(text)}
+            placeholder="영문,숫자,특수문자를 포함한 8자리 초과 20자리 미만"
+            secureTextEntry={true}
+          />
+          <View>
+            {pwFormBlank ? null : pwFormCheck ? null : (
+              <Text style={styles.afterCheck}>
+                비밀번호 양식을 확인해주세요
+              </Text>
+            )}
+          </View>
+          <Text style={styles.titleText}>비밀 번호 확인</Text>
+          <TextInput
+            style={[styles.textInput, {marginBottom: 10}]}
+            placeholder="다시 한번 입력해주세요"
+            secureTextEntry={true}
+            onChangeText={text => {
+              onPasswordConfirmHandler(text);
+            }}
+          />
+          <View>
+            {pwBlank ? null : pwCheck ? null : (
+              <Text style={styles.preCheck}>비밀번호가 일치하지 않습니다!</Text>
+            )}
+          </View>
+        </View>
+        <Text style={styles.titleText}>휴대 전화</Text>
+        <View style={styles.inputWithBtnArea}>
+          <TextInput
+            style={styles.inputWithBtn}
+            autoCompleteType={'tel'}
+            placeholder="010-0000-0000"
+            onChangeText={text => {
+              onCellInputHandler(text);
+            }}
+          />
+          <TouchableOpacity
+            style={styles.CheckBtn}
+            onPress={onCellCheckHandler}>
+            <Text style={styles.btnText}>중복체크</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          {cellBlank ? null : cellCheck ? (
+            isduplicated_cell ? (
+              <Text style={styles.preCheck}>휴대전화가 중복되었습니다.</Text>
+            ) : null
+          ) : (
+            <Text style={styles.preCheck}>휴대전화 중복체크를 해주세요.</Text>
+          )}
+        </View>
+      </View>
+      <View style={styles.loginBtnArea}>
+        <TouchableOpacity
+          style={styles.loginBtn}
+          onPress={checkAllRegisterInfoHandler}>
+          <Text style={styles.loginText}>회원가입</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default RegisterForm;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginLeft: "5%",
-        marginRight: "5%",
-    },
-    inputForm: {
-        paddingTop: "20%",
-    },
-    textInput:{
-        borderRightWidth: 1,
-        borderLeftWidth: 1,
-        borderBottomWidth: 1,
-        borderTopWidth: 1,
-        height: 40,
-        borderColor: "rgb(226,226,226)",
-        marginBottom: 20,
-    },
-    inputWithBtn:{
-        borderRightWidth: 1,
-        borderLeftWidth: 1,
-        borderBottomWidth: 1,
-        borderTopWidth: 1,
-        height: 40,
-        borderColor: "rgb(226,226,226)",
-        marginBottom: 10,
-        width: 270,
-    },
-    preCheck:{
-        fontSize: 16,
-        fontWeight: "bold",
-        color: "black",
-        marginBottom: 20,
-    },
-    afterCheck:{
-        fontSize: 16,
-        color: "rgb(218, 41, 28)",
-        marginBottom: 20,
-    },
-    titleText:{
-        fontSize: 18,
-        marginBottom: 3,
-    },
-    inputWithBtnArea:{
-        flexDirection:"row",
-        justifyContent:"space-between",
-        alignItems:"center",
-    },
-    CheckBtn:{
-        height: 40,
-        alignItems: "center",
-        backgroundColor: 'rgb(218, 41, 28)',
-        marginBottom: 10,
-    },
-    btnText:{
-        fontSize: 17,
-        color: "white",
-        margin: 8,
-    },
-    timerText:{
-        fontSize: 28,
-        color: "rgb(218, 41, 28)",
-        paddingBottom: 14,
-    },
-    loginBtnArea: {
-        marginTop: 10,
-        marginBottom: 10,
-    },
-    loginBtn: {
-        alignItems: "center",
-        backgroundColor: 'rgb(218, 41, 28)',
-    },
-    loginText:{
-        fontSize: 26,
-        color: "white",
-        margin: 6,
-    },
-})
+  container: {
+    flex: 1,
+    marginLeft: '5%',
+    marginRight: '5%',
+  },
+  inputForm: {
+    paddingTop: '20%',
+  },
+  textInput: {
+    borderRightWidth: 1,
+    borderLeftWidth: 1,
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    height: 40,
+    borderColor: 'rgb(226,226,226)',
+    marginBottom: 10,
+  },
+  inputWithBtn: {
+    borderRightWidth: 1,
+    borderLeftWidth: 1,
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    height: 40,
+    borderColor: 'rgb(226,226,226)',
+    marginBottom: 10,
+    width: 270,
+  },
+  preCheck: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'rgb(218, 41, 28)',
+    marginBottom: 20,
+  },
+  afterCheck: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'rgb(218, 41, 28)',
+    marginBottom: 20,
+  },
+  titleText: {
+    fontSize: 18,
+    marginBottom: 3,
+  },
+  inputWithBtnArea: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  CheckBtn: {
+    height: 40,
+    alignItems: 'center',
+    backgroundColor: 'rgb(218, 41, 28)',
+    marginBottom: 10,
+  },
+  btnText: {
+    fontSize: 17,
+    color: 'white',
+    margin: 8,
+  },
+  timerText: {
+    fontSize: 28,
+    color: 'rgb(218, 41, 28)',
+    paddingBottom: 14,
+  },
+  loginBtnArea: {
+    marginTop: 80,
+    marginBottom: 10,
+  },
+  loginBtn: {
+    alignItems: 'center',
+    backgroundColor: 'rgb(218, 41, 28)',
+  },
+  loginText: {
+    fontSize: 26,
+    color: 'white',
+    margin: 6,
+  },
+});
