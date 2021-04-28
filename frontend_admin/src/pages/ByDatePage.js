dc8c57ee2f5d8a92cad5164f3c41931c51daf0e0
곽충섭 <cndtjq145@naver.com>
2021-04-28 16:57:40 +0900

    Update S04P31D101-106 : 관리자 페이지 UI 구현 중

---
+++ frontend_admin/src/pages/ByDatePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ByDatePage = () => {
+    return <div>날짜별 조회</div>
+}
+
+export default ByDatePage
---
+++ frontend_admin/src/pages/StatisticsDatePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StatisticsDatePage = () => {
+    return <div>날짜별 통계</div>
+}
+
+export default StatisticsDatePage
