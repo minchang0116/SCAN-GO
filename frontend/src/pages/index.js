a5a634ae9c296ad67ac529bd3e8569827d21261c
강유정 <kos9811@naver.com>
2021-04-30 19:01:13 +0900

    Add S04P31D101-35 : 결제내역 기간설정 UI

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,10 +1,10 @@
-import PaymentList from './PaymentList';
+import PaymentList from './PaymentListPage';
 import MainPage from './MainPage';
 import BarcodeScanningPage from './BarcodeScanningPage';
 import ShoppingListPage from './ShoppingListPage';
 import ImageScanningPage from './ImageScanningPage';
-import LoginPage from './LoginPage'
-import RegisterForm from './RegisterForm'
+import LoginPage from './LoginPage';
+import RegisterForm from './RegisterForm';
 
 export {
   PaymentList,
