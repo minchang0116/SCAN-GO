1297aa5a2ad1cb3a8e59e4a19dcc16d734f35870
곽충섭 <cndtjq145@naver.com>
2021-05-04 14:23:34 +0900

    Add S04P31D101-41 : 관리자 API 임시 연결

---
+++ frontend_admin/src/lib/api/managing.js
@@ -0,0 +1,16 @@
+import client from './client';
+
+// 최근 판매 가격
+export const getTotalDeposit = () => {
+    return client.get('/');
+}
+
+// 최근 판매 리스트, 페이징
+export const getRecentOrder = (pageNum) => {
+    return client.get(`/?pageNum=${pageNum}`);
+}
+
+// 하루 매출액 -> 시간별
+export const getTodayPrice = () => {
+    return client.get('/');
+}
