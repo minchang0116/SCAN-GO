de69c34791314233eeaf61f695b94d515a6b3ca8
강유정 <kos9811@naver.com>
2021-05-05 21:49:17 +0900

    Add S04P31D101-141 : 앱 아이콘, 이름 변경 & splash screen

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {configureStore} from '@reduxjs/toolkit';
 import rootReducer from './src/modules';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNav from './src/navigations/Stack';
 import {shoppingListApi} from './src/lib/api/shoppingList';
+import SplashScreen from 'react-native-splash-screen';
 
 const store = configureStore({
   reducer: rootReducer,
@@ -18,6 +19,9 @@ const store = configureStore({
 });
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <Provider store={store}>
       <NavigationContainer>
