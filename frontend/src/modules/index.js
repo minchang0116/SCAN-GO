d7fb5e5b870beceb78f102d4d5747a8d1df6024f
곽충섭 <cndtjq145@naver.com>
2021-05-07 14:05:49 +0900

    Update S04P31D101-128 : 유저 API 임시 연결

--- frontend/src/modules/index.js
+++ frontend/src/modules/index.js
@@ -2,14 +2,14 @@ import {combineReducers} from 'redux';
 import loading from './loading';
 import shoppingList from './shoppingList';
 import paymentList from './paymentList';
-import auth from './auth';
+import userInfo from './userInfo';
 import payment from './payment';
 
 const rootReducer = combineReducers({
   loading,
   shoppingList,
   paymentList,
-  auth,
+  userInfo,
   payment,
 });
 
