7aec6553b1538a947f755e679024a0803d428226
곽충섭 <cndtjq145@naver.com>
2021-05-07 14:34:37 +0900

    Update S04P31D101-128 : 유저 API 임시 연결

--- frontend/src/modules/userInfo.js
+++ frontend/src/modules/userInfo.js
@@ -1,53 +1,52 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 import * as authAPI from '../lib/api/auth';
 
-
 export const fetchUserInfo = createAsyncThunk(
-    'userInfo/fetchUserInfo',
-    async (loginInfo) => {
-      try {
-        console.log("유저 정보 요청 dispatch 성공 - ")
-        let response = await authAPI.LoginWithPassword(loginInfo);
-        return response.data;
-      } catch (e) {
-        console.log("유저 정보 요청 dispatch 실패 - ")
-      }
+  'userInfo/fetchUserInfo',
+  async loginInfo => {
+    try {
+      console.log('유저 정보 요청 dispatch 성공 - ');
+      let response = await authAPI.LoginWithPassword(loginInfo);
+      return response.data;
+    } catch (e) {
+      console.log('유저 정보 요청 dispatch 실패 - ');
+    }
+  },
+);
+const userSlice = createSlice({
+  name: 'userInfo',
+  initialState: {
+    loading: false,
+    hasErrors: false,
+    memberId: '',
+    loginId: '',
+    birth: '',
+    phone: '',
+  },
+  reducers: {
+    callUserLogout: () => {
+      state.user = {};
     },
-  );
-  const userSlice = createSlice({
-    name: 'userInfo',
-    initialState: {
-      loading: false,
-      hasErrors: false,
-      user:{},
+  },
+  extraReducers: {
+    [fetchUserInfo.pending]: state => {
+      state.loading = true;
     },
-    reducers: {
-        callUserLogout : () => {
-            state.user = {};
-        }
+    [fetchUserInfo.fulfilled]: (state, {payload}) => {
+      console.log('유저 정보 이행');
+      state.memberId = payload.id;
+      state.loginId = payload.loginId;
+      state.birth = payload.birth;
+      state.phone = payload.phone;
+      state.loading = false;
+      state.hasErrors = false;
     },
-    extraReducers: {
-      [fetchUserInfo.pending]: state => {
-        state.loading = true;
-      },
-      [fetchUserInfo.fulfilled]: (state, {payload}) => {
-        let tmp = {
-          memberId: payload.id,
-          loginId: payload.loginId,
-          birth: payload.birth,
-          phone: payload.phone,
-        }
-        state.user = tmp;
-        console.log("유저 정보 이행 user: " + payload);
-        state.loading = false;
-        state.hasErrors = false;
-      },
-      [fetchUserInfo.rejected]: state => {
-        state.loading = false;
-        state.hasErrors = true;
-      },
+    [fetchUserInfo.rejected]: state => {
+      state.loading = false;
+      state.hasErrors = true;
     },
-  });
-  
-  export const {} =  userSlice.actions;
-  export default userSlice.reducer;
+  },
+});
+
+export const {} = userSlice.actions;
+export default userSlice.reducer;
