d7732eee986f00e4b08145d699cfd67a70205bb4
강유정 <kos9811@naver.com>
2021-05-10 22:16:46 +0900

    Add S04P31D101-26, S04P31D101-25 : 상품 이미지 인식 기능 상품 이미지 인식 후 처리

--- frontend/src/pages/BarcodeScanningPage.js
+++ frontend/src/pages/BarcodeScanningPage.js
@@ -1,21 +1,17 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useEffect, useState} from 'react';
-import {
-  SafeAreaView,
-  View,
-  StyleSheet,
-  TouchableOpacity,
-  ToastAndroid,
-} from 'react-native';
+import {View, StyleSheet, TouchableOpacity, ToastAndroid} from 'react-native';
 import {CameraScreen} from 'react-native-camera-kit';
 import {CameraFooter} from '../components/scanning/CameraFooter';
 import IconAntD from 'react-native-vector-icons/AntDesign';
+import IconF from 'react-native-vector-icons/Feather';
 import CameraItem from '../components/scanning/CameraItem';
 import {useDispatch, useSelector} from 'react-redux';
 import {
   addShoppingListItemByBarcode,
   removeLastItem,
 } from '../modules/shoppingList';
+import AppText from '../components/common/AppText';
 
 const BarcodeScanningPage = ({navigation}) => {
   const dispatch = useDispatch();
@@ -59,67 +55,38 @@ const BarcodeScanningPage = ({navigation}) => {
   }, [lastItem]);
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <View style={{flex: 1}}>
+      <TouchableOpacity
+        style={styles.close}
+        onPress={() => navigation.navigate('MainPage')}>
+        <IconAntD name="close" size={30} color="rgb(255, 255, 255)" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.imageBtn}
+        onPress={() => navigation.navigate('ImageScanningPage')}>
+        <IconF name="camera" size={30} color="rgb(255, 255, 255)" />
+        <AppText style={{color: 'rgb(255,255,255)'}}>상품이미지</AppText>
+        <AppText style={{color: 'rgb(255,255,255)'}}>스캔</AppText>
+      </TouchableOpacity>
       <View style={{flex: 1}}>
-        <TouchableOpacity
-          style={styles.close}
-          onPress={() => navigation.navigate('MainPage')}>
-          <IconAntD name="close" size={30} color="rgb(255, 255, 255)" />
-        </TouchableOpacity>
-        <View style={{flex: 1}}>
-          <CameraScreen
-            showFrame={true}
-            scanBarcode={true}
-            laserColor={'transparent'}
-            frameColor={'red'}
-            colorForScannerFrame={'white'}
-            onReadCode={event => setQrvalue(event.nativeEvent.codeStringValue)}
-          />
-        </View>
-        {lastItem && <CameraItem lastItem={lastItem} />}
-        <CameraFooter navigation={navigation} sumPrice={sumPrice} />
+        <CameraScreen
+          showFrame={true}
+          scanBarcode={true}
+          laserColor={'transparent'}
+          frameColor={'red'}
+          colorForScannerFrame={'white'}
+          onReadCode={event => setQrvalue(event.nativeEvent.codeStringValue)}
+        />
       </View>
-    </SafeAreaView>
+      {lastItem && <CameraItem lastItem={lastItem} style={styles.card} />}
+      <CameraFooter navigation={navigation} sumPrice={sumPrice} />
+    </View>
   );
 };
 
 export default BarcodeScanningPage;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    padding: 10,
-    alignItems: 'center',
-  },
-  titleText: {
-    fontSize: 22,
-    textAlign: 'center',
-    fontWeight: 'bold',
-  },
-  textStyle: {
-    color: 'black',
-    fontSize: 16,
-    textAlign: 'center',
-    padding: 10,
-    marginTop: 16,
-  },
-  addBtnStyle: {
-    fontSize: 16,
-    color: 'white',
-    backgroundColor: 'green',
-    padding: 5,
-    minWidth: 250,
-  },
-  buttonTextStyle: {
-    padding: 5,
-    color: 'white',
-    textAlign: 'center',
-  },
-  textLinkStyle: {
-    color: 'blue',
-    paddingVertical: 20,
-  },
   close: {
     zIndex: 5,
     width: 50,
@@ -128,17 +95,24 @@ const styles = StyleSheet.create({
     left: '88%',
     top: '3%',
   },
-  errorContainer: {
-    backgroundColor: 'white',
-    position: 'absolute',
+  imageBtn: {
+    backgroundColor: 'rgb(218,41,28)',
     zIndex: 5,
-    top: 100,
-    left: 20,
-    right: 20,
-    borderRadius: 100,
-    height: 40,
-    flexDirection: 'row',
+    // width: 60,
+    // height: 60,
+    position: 'absolute',
+    left: '75%',
+    top: '10%',
+    borderRadius: 6,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  card: {
+    zIndex: 5,
+    width: '95%',
+    marginRight: 'auto',
+    marginBottom: 40,
+    marginLeft: 'auto',
+    borderRadius: 10,
   },
 });
