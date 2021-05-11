45cd0437d05ea221fd44d82f3fe980fa149ba0bf
leegw217 <leegw217@naver.com>
2021-05-12 01:03:51 +0900

    Update S04P31D101-123 : 회원관련 API 수정

--- backend/SSG/src/main/java/com/ssg/config/SwaggerConfig.java
+++ backend/SSG/src/main/java/com/ssg/config/SwaggerConfig.java
@@ -5,16 +5,22 @@ import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration {
+public class SwaggerConfig {
     //	Swagger 설정 확인
     //	http://localhost:8000/{your-app-root}/v2/api-docs
     //	http://localhost:8000/guestbook/v2/api-docs?group=V1
@@ -28,15 +34,24 @@ public class SwaggerConfiguration {
 
     @Bean
     public Docket api() {
+        ParameterBuilder aParameterBuilder = new ParameterBuilder();
+        aParameterBuilder.name("Authorization") //헤더 이름
+                .description("Access Tocken") //설명
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
+
+        List<Parameter> aParameters = new ArrayList<>();
+        aParameters.add(aParameterBuilder.build());
+
         return new Docket(DocumentationType.SWAGGER_2)
+                //.globalOperationParameters(aParameters)
                 .apiInfo(this.apiInfo())
                 .select()
                 .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
-                .paths(PathSelectors.any()).build();
-    }
-
-    private Predicate<String> postPaths() {
-        return PathSelectors.any();
+                .paths(PathSelectors.any())
+                .build();
     }
 
     private ApiInfo apiInfo() {
