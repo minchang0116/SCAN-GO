38d5657119b827ca5086e9236c4af4d417877b76
곽충섭 <cndtjq22@gmail.com>
2021-05-11 14:34:32 +0900

    Update S04P31D101-19 : 관리자 로그인 페이지 UI 구현. API 연결 중.

--- frontend_admin/src/App.js
+++ frontend_admin/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
-import {
-  SearchPage,
-  Dashboard
-} from './pages';
+import {SearchPage, Dashboard, LoginPage} from './pages';
 
 function App() {
   return (
     <>
-      <Route component={Dashboard} path={['/']} exact />
+      <Route component={LoginPage} path={['/', '/login']} exact />
+      <Route component={Dashboard} path={['/dashboard']} exact />
       <Route component={SearchPage} path={['/search']} exact />
     </>
   );
