ca87c36a68e7577e4ce4fb53aaf26eda026e461b
leegw217 <leegw217@naver.com>
2021-05-13 14:24:19 +0900

    Update S04P31D101-123 : 회원관련 API 수정

--- backend/SSG/src/main/java/com/ssg/member/controller/MemberController.java
+++ backend/SSG/src/main/java/com/ssg/member/controller/MemberController.java
@@ -14,7 +14,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
