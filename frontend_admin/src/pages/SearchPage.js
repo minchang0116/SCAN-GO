fe94f379aaf8d39767e91fe46676334a63fa523c
곽충섭 <cndtjq22@gmail.com>
2021-05-13 13:28:26 +0900

    Update S04P31D101-27 : 관리자 페이지 UI 수정 (데이터피커 추가, 테이블 항목 수정) / 글꼴 변경 / API 연결 중 / 모달 추가 구현 중

--- frontend_admin/src/pages/SearchPage.js
+++ frontend_admin/src/pages/SearchPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
 import Outline from '../Components/outline/Outline';
 import Copyright from '../Components/outline/Copyright';
 import useStyles from '../css/useStyles';
@@ -17,9 +16,6 @@ export default function Dashboard() {
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
           <SearchContainer />
-          <Grid container spacing={3}>
-            {/* 내용 입력 */}
-          </Grid>
           <Box pt={4}>
             <Copyright />
           </Box>
