edcf9779125ef0eb3c2f9dd46b0edbd94402cb5d
강민창 <new0822@naver.com>
2021-05-14 10:40:47 +0900

    Update S04P31D101-165 메인페이지 UI 수정 5

--- frontend/src/components/main/EventProductItem.js
+++ frontend/src/components/main/EventProductItem.js
@@ -9,16 +9,12 @@ const EventProductItem = ({item, home}) => {
     <>
       <View style={styles.wrap}>
         <Thumbnail
-          square
-          large
           style={styles.image}
           source={{
             uri: (home ? 'data:image/png;base64,' : '') + item.image,
           }}
         />
-        <AppText numberOfLines={2} ellipsizeMode="tail" style={styles.prodName}>
-          {item.prodName}
-        </AppText>
+        <AppText style={styles.prodName}>{item.prodName.trim()}</AppText>
         {item.prodSalePrice && (
           <AppText
             numberOfLines={1}
@@ -51,24 +47,24 @@ const styles = StyleSheet.create({
   },
   image: {
     width: 140,
-    height: 150,
+    height: 140,
     resizeMode: 'stretch',
     borderRadius: 10,
   },
   prodName: {
+    marginTop: 5,
+    height: 30,
     fontSize: 12,
-    width: 140,
-    height: 35,
+    width: 130,
   },
   prodPrice: {
-    fontSize: 10,
+    fontSize: 11,
     marginTop: 5,
     textDecorationLine: 'line-through',
     color: 'rgb(144,144,144)',
   },
   prodSalePrice: {
     fontSize: 14,
-    marginTop: 5,
     fontWeight: '700',
   },
 });
