b91fcdb4ddc13f8cf8f4c59d2ca7a5260ffcc597
강유정 <kos9811@naver.com>
2021-05-17 12:31:35 +0900

    Add S04P31D101-170 : 결제내역 페이지 이미지 연결 Update S04P31D101-6 : 인기상품 뱃지 색상변경, 행사상품 가격 표시 변경

--- frontend/src/components/paymentList/ProductItemInModal.js
+++ frontend/src/components/paymentList/ProductItemInModal.js
@@ -5,27 +5,32 @@ import AppText from '../common/AppText';
 import {Thumbnail} from 'native-base';
 
 const ProductItemInModal = ({product}) => {
+  console.log(product);
   return (
     <>
       <View style={styles.wrap}>
         <Thumbnail
           square
-          large
+          style={styles.prodImage}
           source={{
-            uri:
-              'http://image3.compuzone.co.kr/img/product_img/2021/0219/764689/764689_600.jpg',
+            uri: 'data:image/png;base64,' + product.item.prodImage,
           }}
         />
-        <AppText numberOfLines={2} ellipsizeMode="tail" style={styles.font14}>
-          {product.item.prodName}
-        </AppText>
-        <AppText
-          numberOfLines={1}
-          ellipsizeMode="tail"
-          style={{...styles.font14, marginTop: 5}}>
-          {parseInt(product.item.prodPrice, 10).toLocaleString()}원 ·{' '}
-          {product.item.qty}개
-        </AppText>
+        <View style={styles.prodInfo}>
+          <AppText
+            numberOfLines={2}
+            ellipsizeMode="tail"
+            style={{...styles.font12, ...styles.prodName}}>
+            {product.item.prodName}
+          </AppText>
+          <AppText
+            numberOfLines={1}
+            ellipsizeMode="tail"
+            style={{...styles.font12, ...styles.prodPrice}}>
+            {Number(product.item.prodPrice).toLocaleString()}원 ·{' '}
+            {product.item.qty}개
+          </AppText>
+        </View>
       </View>
     </>
   );
@@ -36,12 +41,27 @@ export default ProductItemInModal;
 const styles = StyleSheet.create({
   wrap: {
     width: '33.33%',
-    height: '100%',
+    height: 160,
     paddingRight: 8,
     paddingLeft: 8,
-    // marginBottom: -50,
   },
-  font14: {
-    fontSize: 14,
+  font12: {
+    fontSize: 12,
+  },
+  prodImage: {
+    resizeMode: 'contain',
+    width: '100%',
+    height: 100,
+  },
+  prodInfo: {
+    marginTop: 5,
+    height: 50,
+  },
+  prodName: {
+    height: 30,
+  },
+  prodPrice: {
+    marginTop: 5,
+    fontWeight: '700',
   },
 });
