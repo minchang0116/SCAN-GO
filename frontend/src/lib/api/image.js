884344ce204080d9697de0253e56678085a0a520
seoyounji <wltjdus0208@naver.com>
2021-05-17 19:46:27 +0900

    Update S04P31D101-50 : gpu 서버 - frontend 연동

--- frontend/src/lib/api/image.js
+++ frontend/src/lib/api/image.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getBarcode = formData => {
-  return axios.post('http://3.18.221.184:5000/predict', formData, {
+  return axios.post('http://3.141.244.118:5000/predict', formData, {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
