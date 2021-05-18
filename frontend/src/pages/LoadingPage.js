26a74b91136c5c32d9d8557d2ca2537dc8f71f9c
강민창 <new0822@naver.com>
2021-05-18 18:29:14 +0900

    Add S04P31D101-174 유저 로딩 변경

--- frontend/src/pages/LoadingPage.js
+++ frontend/src/pages/LoadingPage.js
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps*/
 import React, {useCallback} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, ToastAndroid, View} from 'react-native';
 import * as asyncStorage from '../AsyncStorage/asyncStorage';
 import {loadToken} from '../lib/api/client';
 import {useFocusEffect} from '@react-navigation/core';
@@ -11,39 +11,40 @@ import {fetchUserInfoWithAsyncStorage} from '../modules/userInfo';
 const LoadingPage = ({navigation}) => {
   useFocusEffect(
     useCallback(() => {
-      const checkTokenAndMovePage = async () => {
-        if ((await asyncStorage.getData('token')) === false) {
-          console.log('Loading페이지 토큰 없음');
-          // 토큰 없으면 자동 로그인 활성화 안함. 있던 유저 정보 제거
-          await asyncStorage.removeValue('user');
-          // LoginPage로
-          navigation.navigate('LoginPage');
-        } else {
-          // 토큰 있는 경우에는 유저 정보도 있다. MainPage로
-          console.log('Loading페이지 토큰 있음');
-          await loadToken();
-          navigation.navigate('MainPage');
-        }
-      };
-
       checkTokenAndMovePage();
-      checkUserInfo();
     }, []),
   );
 
   const dispatch = useDispatch();
-
-  const checkUserInfo = async () => {
-    let userData = await asyncStorage.getObjectData('user');
-    if (userData !== false) {
-      console.log('유저 정보 Async 존재');
-      console.dir(userData);
-      let response = await dispatch(fetchUserInfoWithAsyncStorage({userData}));
-      if (response.payload === false) {
-        console.log('async 에러');
-      }
+  const checkTokenAndMovePage = async () => {
+    if ((await asyncStorage.getData('token')) === false) {
+      console.log('Loading페이지 토큰 없음');
+      // 토큰 없으면 자동 로그인 활성화 안함. 있던 유저 정보 제거
+      await asyncStorage.removeValue('user');
+      // LoginPage로
+      navigation.navigate('LoginPage');
     } else {
-      console.log('저장된 유저 정보 없음! 로그인 필요!');
+      // 토큰 있는 경우에는 유저 정보도 있다. MainPage로
+      const userData = await asyncStorage.getObjectData('user');
+      if (userData !== false) {
+        ToastAndroid.show('유저 정보 Async 존재', ToastAndroid.SHORT);
+        console.dir(userData);
+        let response = await dispatch(
+          fetchUserInfoWithAsyncStorage({userData}),
+        );
+        if (response.payload === false) {
+          ToastAndroid.show('async 에러', ToastAndroid.SHORT);
+        }
+      } else {
+        ToastAndroid.show(
+          '저장된 유저 정보 없음! 로그인 필요!',
+          ToastAndroid.SHORT,
+        );
+        navigation.navigate('LoginPage');
+      }
+
+      await loadToken();
+      navigation.navigate('MainPage');
     }
   };
 
