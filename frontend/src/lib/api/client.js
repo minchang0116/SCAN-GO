a1096db3122a5e9450e6956e86b5418f15e1f3af
강유정 <kos9811@naver.com>
2021-05-18 16:06:26 +0900

    Update S04P31D101-140 : api 주소 변경

--- frontend/src/lib/api/client.js
+++ frontend/src/lib/api/client.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import * as asyncStorage from '../../AsyncStorage/asyncStorage';
 
-const host = 'http://k4d101.p.ssafy.io:9000/ssg';
+const host = 'https://k4d101.p.ssafy.io/ssg';
 export const loadToken = async () => {
   client.defaults.headers.common['Authorization'] = await asyncStorage.getData(
     'token',
   );
 };
+console.log(asyncStorage.getData('token'));
 const client = axios.create({
   baseURL: host,
   withCredentials: true,
