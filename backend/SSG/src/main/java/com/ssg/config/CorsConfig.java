0d46c6c42cf368232bbeb976b9ef1d2f4cc42aac
leegw217 <leegw217@naver.com>
2021-05-18 17:55:56 +0900

    ADMIN 페이지 배포 test

--- backend/SSG/src/main/java/com/ssg/config/CorsConfig.java
+++ backend/SSG/src/main/java/com/ssg/config/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.addAllowedOrigin("https://k4d101.p.ssafy.io");
         config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedHeader("*");
+        config.addAllowedHeader("https://k4d101.p.ssafy.io");
+        config.addAllowedHeader("http://localhost:3000");
         config.addAllowedMethod("*");
 
         source.registerCorsConfiguration("/member/**", config);
