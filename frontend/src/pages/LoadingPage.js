2ef805cc7e6e153beec72994ef38d6826a93050e
곽충섭 <cndtjq145@naver.com>
2021-05-18 19:15:47 +0900

    FE / Loading 페이지 수정. 어플 껐다가 킨 경우 로그인 정보 불러오기 수정

--- frontend/src/pages/LoadingPage.js
+++ frontend/src/pages/LoadingPage.js
@@ -9,8 +9,11 @@ import {useDispatch} from 'react-redux';
 import {fetchUserInfoWithAsyncStorage} from '../modules/userInfo';
 
 const LoadingPage = ({navigation}) => {
+  const dispatch = useDispatch();
+
   useFocusEffect(
     useCallback(() => {
+<<<<<<< Updated upstream
       checkTokenAndMovePage();
     }, []),
   );
@@ -48,6 +51,33 @@ const LoadingPage = ({navigation}) => {
     }
   };
 
+=======
+      const checkTokenAndMovePage = async () => {
+        if (!(await asyncStorage.getData('token'))) {
+          console.log('Loading페이지 토큰 없음');
+          // 토큰 없으면 자동 로그인 활성화 안함. 있던 유저 정보 제거
+          await asyncStorage.removeValue('user');
+          // LoginPage로
+          navigation.navigate('LoginPage');
+        } else {
+          // 토큰 있는 경우에는 유저 정보도 있다. MainPage로
+          console.log('Loading페이지 토큰 있음');
+          await loadToken();
+          let userData = await asyncStorage.getObjectData('user');
+          let response = await dispatch(
+            fetchUserInfoWithAsyncStorage({userData: userData}),
+          );
+          console.log('유저 정보 불러옴')
+          console.log(response.payload);
+          navigation.navigate('MainPage');
+        }
+      };
+
+      checkTokenAndMovePage();
+    }, []),
+  );
+  
+>>>>>>> Stashed changes
   return (
     <>
       <View style={styles.container}>
